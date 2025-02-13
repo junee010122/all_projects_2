db6cf714e1eae2ef7a27de44a8bce327c43a2677
Junyoung Shin <jsdcn@umsystem.edu>
2025-02-13 12:04:39 -0600

    update data.py

--- quality_assurance/utils/data.py
+++ quality_assurance/utils/data.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import mysql.connector
+import tqdm
 from sqlalchemy import create_engine
 
-def 
+def evaluate_preprocessing(X, y, strategy_name):
+    X = X.select_dtypes(include=["number"])  # Ensure only numeric columns are used
+    model = RandomForestClassifier(n_estimators=50, random_state=42, n_jobs=-1)
+    model.fit(X, y)
+    return model.score(X, y)
+
+def handle_missing_values(X, y, numeric_cols):
+    pass
+
+def handle_feature_selection(X, y):
+    pass
+
+def handle_normalization(X, y):
+    pass
+
+def handle_imbalance():
+    pass
 
 def create_SQL(data):
 
@@ -34,6 +51,13 @@ def create_SQL(data):
 
 
 def preprocess_data(data, params):
+
+    # read in params
+    impute_strategy = params['preprocessing']['missing_value']
+    feature_selection_strategy = params['preprocessing']['feature_selection']
+    normalization_strategy = params['preprocessing']['normalization']
+    imbalance_strategy = params['preprocessing']['imbalance']
+
     
     create_SQL(data)
     df = data
@@ -51,7 +75,8 @@ def preprocess_data(data, params):
 
     with tqdm(total=5, desc='Preprocessing Data') as pbar:
         
-        handle_missing_values()
-        handle_feature_selection()
-        handle_normalization()
+        handle_missing_values(impute_strategy, X, y, numeric_cols)
+        handle_feature_selection(feature_selection_strategy, X, y)
+        handle_normalization(normalization_strategy, X, y)
+        handle_imbalance(imbalance_strategy, X, y)
     
